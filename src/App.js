31fd9c7d12cc6c5aedebdeed845c283776f05f3c
WalkanFL <maciej.b.szczepanski@o2.pl>
2023-01-21 15:09:17 +0000

    not even anything worth the push but i want to have this documented

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Random Anime Quotes</h1>
+      <h2>Start editing to seee some magic happen!</h2>
+      <button onClick="quotes"> Get your quote </button>
+      <p id="quoteSpace"></p>
     </div>
   );
+}
+
+function quotes() {
+  fetch("https://animechan.vercel.app/api/random")
+    .then((response) => response.json())
+    .then((quote) => console.log(quote))
+    .then(document.write(quote.quote));
 }
